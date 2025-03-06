9ac2b1ff673c48ced48c70c325acfdf09c1171e1
Johannes Bauer <jbauer@implen.de>
2025-03-06 09:56:21 +0100

    build: Remove hello_generated.cpp from build

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 flatc --cpp hello.fbs
 
 # Build the project
-g++ -std=c++11 main.cpp rpc.cpp hello_generated.cpp -o example -lpthread -lboost_coroutine -lboost_context
+g++ -std=c++11 main.cpp rpc.cpp -o example -lpthread -lboost_coroutine -lboost_context
 
 # Run the example
 ./example
