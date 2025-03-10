d12093a7ee1c8e2e99d415c05f7e9d5921149432
Johannes Bauer <jbauer@implen.de>
2025-03-10 11:50:28 +0100

    Create own build file for client

--- build.sh
+++ build.sh
@@ -8,8 +8,3 @@ rm -f c++/hello_generated.h
 mv hello_generated.h c++
 rm -rf go/Hello
 mv -u Hello go
-
-# Build Go client and server binaries
-mkdir -p go/client/bin
-cd go/server && go build -o ../bin/rpc_server .
-cd ../../go/client && go build -o ../bin/rpc_client .
