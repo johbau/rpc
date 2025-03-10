8d5b5583f309fa30f24f4481d743f0662575de36
Johannes Bauer <jbauer@implen.de>
2025-03-10 19:13:36 +0100

    Refactor code

--- go/client/main.go
+++ go/client/main.go
@@ -10,23 +10,6 @@ import (
 )
 
 func main() {
-	// Create or open the client message queue
-	const clientFlags = posix_mq.O_RDONLY | posix_mq.O_CREAT
-	clientMq, err := posix_mq.NewMessageQueue("/client_queue_hello", clientFlags, 0666, nil)
-	if err != nil {
-		log.Fatalf("Failed to create client message queue: %v", err)
-	}
-	defer clientMq.Close()
-	defer clientMq.Unlink()
-
-	// Create or open the server message queue
-	const serverFlags = posix_mq.O_WRONLY
-	serverMq, err := posix_mq.NewMessageQueue("/server_queue_hello", serverFlags, 0, nil)
-	if err != nil {
-		log.Fatalf("Failed to open server message queue: %v", err)
-	}
-	defer serverMq.Close()
-
 	// Create a FlatBuffer request
 	builder := flatbuffers.NewBuilder(1024)
 	message := builder.CreateString("Hello from Go client!")
@@ -36,11 +19,27 @@ func main() {
 	request := Hello.HelloRequestEnd(builder)
 	builder.Finish(request)
 
+	// Create or open the server message queue
+	const serverFlags = posix_mq.O_WRONLY
+	serverMq, err := posix_mq.NewMessageQueue("/server_queue_hello", serverFlags, 0, nil)
+	if err != nil {
+		log.Fatalf("Failed to open server message queue: %v", err)
+	}
+	defer serverMq.Close()
+
 	// Send request to server
 	err = serverMq.Send(builder.FinishedBytes(), 0)
 	if err != nil {
 		log.Fatalf("Failed to send request: %v", err)
 	}
+
+	// Create or open the client message queue
+	const clientFlags = posix_mq.O_RDONLY | posix_mq.O_CREAT
+	clientMq, err := posix_mq.NewMessageQueue("/client_queue_hello", clientFlags, 0666, nil)
+	if err != nil {
+		log.Fatalf("Failed to create client message queue: %v", err)
+	}
+	defer clientMq.Unlink()
 
 	// Receive response
 	msg, _, err := clientMq.Receive()
@@ -52,5 +51,6 @@ func main() {
 	response := Hello.GetRootAsHelloResponse(msg, 0)
 	length := response.ResultLength()
 	if length > 0 {
+		fmt.Printf("Received response: %s\n", string(response.Result(0)))
 	}
 }
