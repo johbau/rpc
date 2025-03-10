625cbdd0d0791ee07d94ea42b0c1298aa4bb2064
Johannes Bauer <jbauer@implen.de>
2025-03-10 16:25:14 +0100

    Make changes in order to compile

--- go/client/main.go
+++ go/client/main.go
@@ -1,23 +1,17 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"log"
 
 	"github.com/aceofkid/posix_mq"
-	"github.com/google/flatbuffers"
+	"github.com/google/flatbuffers/go"
 	"github.com/johbau/rpc/go/Hello"
-)
-
-const (
-	serviceName = "hello"
-	queueName   = "/client_queue_hello"
 )
 
 func main() {
 	// Create or open the client message queue
-	clientMq, err := posix_mq.NewMessageQueue(queueName, posix_mq.O_RDONLY | posix_mq.O_CREAT, 0666, nil)
+	clientMq, err := posix_mq.NewMessageQueue("/client_queue_hello", posix_mq.O_RDONLY|posix_mq.O_CREAT, 0666, nil)
 	if err != nil {
 		log.Fatalf("Failed to create client message queue: %v", err)
 	}
@@ -25,7 +19,7 @@ func main() {
 	defer clientMq.Unlink()
 
 	// Create or open the server message queue
-	serverMq, err := posix_mq.NewMessageQueue("/server_queue_hello", posix_mq.O_WRONLY | posix_mq.O_CREAT, 0666, nil)
+	serverMq, err := posix_mq.NewMessageQueue("/server_queue_hello", posix_mq.O_WRONLY|posix_mq.O_CREAT, 0666, nil)
 	if err != nil {
 		log.Fatalf("Failed to open server message queue: %v", err)
 	}
@@ -34,24 +28,28 @@ func main() {
 	// Create a FlatBuffer request
 	builder := flatbuffers.NewBuilder(1024)
 	Hello.HelloRequestStart(builder)
-	Hello.HelloRequestAddGreetingStr(builder, "Hello from Go client!")
+	message := builder.CreateString("Hello from Go client!")
+	Hello.HelloRequestAddRequestType(builder, Hello.RequestTypeGREETING)
+	Hello.HelloRequestAddMessage(builder, message)
 	request := Hello.HelloRequestEnd(builder)
 	builder.Finish(request)
 
 	// Send request to server
-	_, err = serverMq.Send(context.Background(), builder.Data[0:request.BytesWritten()], 0)
+	err = serverMq.Send(builder.FinishedBytes(), 0)
 	if err != nil {
 		log.Fatalf("Failed to send request: %v", err)
 	}
 
 	// Receive response
-	response := make([]byte, 1024)
-	_, err = clientMq.Receive(context.Background(), response, 0)
+	msg, _, err := clientMq.Receive()
 	if err != nil {
 		log.Fatalf("Failed to receive response: %v", err)
 	}
 
 	// Parse the response
-	root := Hello.GetRootAsHelloResponse(bytes.NewBuffer(response))
-	log.Printf("Received response: %s", root.Greeting())
+	response := Hello.GetRootAsHelloResponse(msg, 0)
+	length := response.ResultLength()
+	if length > 0 {
+		fmt.Printf("Received response: %s", string(response.Result(0)))
+	}
 }
