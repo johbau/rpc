bc22705729f8c24c3d587932b2ab30e9e930ec58
Johannes Bauer <jbauer@implen.de>
2025-03-11 15:55:00 +0100

    Remove flatbuffers generation

--- c++/client/build.sh
+++ c++/client/build.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-# Generate FlatBuffers schema
-flatc --cpp ../hello.fbs
-flatc --go ../hello.fbs
-
 # Build the project
 g++ -std=c++11 main.cpp rpc.cpp -o client -lpthread -lboost_system -lrt
